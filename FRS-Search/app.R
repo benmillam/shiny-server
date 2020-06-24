3c1854b5d1d045d50d7977f3a1c21a76a5f302dd
benmillam <bmillam@gmail.com>
2020-06-24 14:33:47 +0000

    make B production table 2020-05-18 data update

--- FRS-Search/app.R
+++ FRS-Search/app.R
@@ -148,7 +148,7 @@ server <- function(input, output, session) {
         county_text_to_match <- input$county_text
         
         dbi_result <- dbSendQuery(conn,
-          "SELECT * FROM FrsFacility 
+          "SELECT * FROM FrsFacilityB 
            WHERE state = ? AND county LIKE ?
            ;")
         
